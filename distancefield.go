e9fcd11e4c233cba6ee1c0c86046963e3151fb1b
Jesper Hansen <jesper@jesperhansen.net>
2016-01-25 07:07:53 +0100

    Added outline command.

--- distancefield.go
+++ distancefield.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"fmt"
 	"image"
@@ -20,13 +21,13 @@ func NewFieldFromImage(image *image.Gray) *Field {
 	internalField := newFieldFromImage(image)
 	internalField.fill()
 
-	field := &Field{Width:image.Bounds().Dx(), Height:image.Bounds().Dy()}
-	field.Field = make([]Point, field.Width * field.Height)
+	field := &Field{Width: image.Bounds().Dx(), Height: image.Bounds().Dy()}
+	field.Field = make([]Point, field.Width*field.Height)
 
 	for y := 0; y < field.Height; y++ {
 		for x := 0; x < field.Width; x++ {
-			soff := (x + 1) + (y + 1) * internalField.Width
-			doff := x + y * field.Width
+			soff := (x + 1) + (y+1)*internalField.Width
+			doff := x + y*field.Width
 			field.Field[doff].InsideDist = math.Sqrt(float64(internalField.Field[soff].ISqDist))
 			field.Field[doff].OutsideDist = math.Sqrt(float64(internalField.Field[soff].OSqDist))
 		}
@@ -40,36 +41,36 @@ func (field *Field) CreateDebugImage() image.Image {
 
 	for y := 0; y < field.Height; y++ {
 		for x := 0; x < field.Width; x++ {
-			p := field.Field[x + y * field.Width]
+			p := field.Field[x+y*field.Width]
 
-			imgOff := x * 4 + y * img.Stride
-			img.Pix[imgOff + 0] = 0
-			img.Pix[imgOff + 1] = 0
-			img.Pix[imgOff + 2] = 0
-			img.Pix[imgOff + 3] = 255
+			imgOff := x*4 + y*img.Stride
+			img.Pix[imgOff+0] = 0
+			img.Pix[imgOff+1] = 0
+			img.Pix[imgOff+2] = 0
+			img.Pix[imgOff+3] = 255
 
 			if p.InsideDist == p.OutsideDist {
 				//fmt.Println(p.InsideDist)
-				img.Pix[imgOff + 1] = 255
+				img.Pix[imgOff+1] = 255
 			} else if p.InsideDist > p.OutsideDist {
 				if math.Mod(p.InsideDist, 5) <= 2.5 {
-					img.Pix[imgOff + 0] = 255
+					img.Pix[imgOff+0] = 255
 				}
 
 				/*if p.InsideDist <= 1 {
 					img.Pix[imgOff + 0] = 255
 				}*/
 				/*else if p.InsideDist >= 5 {
-								   if math.Mod(p.InsideDist, 5) <= 2.5 {
-									   img.Pix[imgOff + 0] = 255
-								   }
-							   }*/
+					   if math.Mod(p.InsideDist, 5) <= 2.5 {
+						   img.Pix[imgOff + 0] = 255
+					   }
+				   }*/
 			} else {
 				//				if p.OutsideDist <= 1 {
 				//					img.Pix[imgOff + 2] = 255
 				//				}
 				if math.Mod(p.OutsideDist, 5) <= 2.5 {
-					img.Pix[imgOff + 2] = 255
+					img.Pix[imgOff+2] = 255
 				}
 			}
 		}
@@ -79,12 +80,12 @@ func (field *Field) CreateDebugImage() image.Image {
 }
 
 func (srcField *Field) ScaleDown(factor int) *Field {
-	if (srcField.Width % factor) != 0 || (srcField.Height % factor) != 0 {
+	if (srcField.Width%factor) != 0 || (srcField.Height%factor) != 0 {
 		panic("invalid size or scale factor")
 	}
 
-	dstField := &Field{Width:srcField.Width / factor, Height:srcField.Height / factor}
-	dstField.Field = make([]Point, dstField.Width * dstField.Height)
+	dstField := &Field{Width: srcField.Width / factor, Height: srcField.Height / factor}
+	dstField.Field = make([]Point, dstField.Width*dstField.Height)
 
 	for y := 0; y < dstField.Height; y++ {
 		for x := 0; x < dstField.Width; x++ {
@@ -97,7 +98,7 @@ func (srcField *Field) ScaleDown(factor int) *Field {
 
 			for sy := 0; sy < factor; sy++ {
 				for sx := 0; sx < factor; sx++ {
-					soff := (x * factor + sx ) + (y * factor + sy ) * srcField.Width
+					soff := (x*factor + sx) + (y*factor+sy)*srcField.Width
 					//					if srcField.Field[soff].OutsideDist < bestOutside {
 					//						bestOutside = srcField.Field[soff].OutsideDist
 					//					}
@@ -109,50 +110,49 @@ func (srcField *Field) ScaleDown(factor int) *Field {
 				}
 			}
 
-			doff := x + y * dstField.Width
+			doff := x + y*dstField.Width
 			//			dstField.Field[doff].OutsideDist = bestOutside / float64(factor)
 			//			dstField.Field[doff].InsideDist = bestInside / float64(factor)
-			dstField.Field[doff].OutsideDist = sumOutside / float64(factor * factor * factor)
-			dstField.Field[doff].InsideDist = sumInside / float64(factor * factor * factor)
+			dstField.Field[doff].OutsideDist = sumOutside / float64(factor*factor*factor)
+			dstField.Field[doff].InsideDist = sumInside / float64(factor*factor*factor)
 		}
 	}
 
 	return dstField
 }
 
-
 func NewMonochromeFromTreshold(srcImage image.Image, grayThreshold, alphaThreshold int) *image.Gray {
-	width := srcImage.Bounds().Dx();
-	height := srcImage.Bounds().Dy();
+	width := srcImage.Bounds().Dx()
+	height := srcImage.Bounds().Dy()
 
 	dstImage := image.NewGray(srcImage.Bounds())
 
 	grayNRGBAThreshold := grayThreshold * ((54 + 184 + 18) * 255) // 255 = because rgb is multiplied with alpha [0;255]
-	grayRGBAThreshold := grayThreshold * ((54 + 184 + 18 ))
+	grayRGBAThreshold := grayThreshold * (54 + 184 + 18)
 
 	switch srcImage := srcImage.(type) {
 	case *image.NRGBA:
 		for y := 0; y < height; y++ {
 			for x := 0; x < width; x++ {
-				off := (x * 4 + y * srcImage.Stride)
-				a := int(srcImage.Pix[off + 3])
-				g := (int(srcImage.Pix[off + 0]) * 54 + int(srcImage.Pix[off + 1]) * 184 + int(srcImage.Pix[off + 2]) * 18) * a
+				off := (x*4 + y*srcImage.Stride)
+				a := int(srcImage.Pix[off+3])
+				g := (int(srcImage.Pix[off+0])*54 + int(srcImage.Pix[off+1])*184 + int(srcImage.Pix[off+2])*18) * a
 
 				if a >= alphaThreshold || g >= grayNRGBAThreshold {
 					//fmt.Println(a, g, grayNRGBAThreshold)
-					dstImage.Pix[x + y * dstImage.Stride] = 255
+					dstImage.Pix[x+y*dstImage.Stride] = 255
 				}
 			}
 		}
 	case *image.RGBA:
 		for y := 0; y < height; y++ {
 			for x := 0; x < width; x++ {
-				off := (x * 4 + y * srcImage.Stride)
-				a := int(srcImage.Pix[off + 3])
-				g := int(srcImage.Pix[off + 0]) * 54 + int(srcImage.Pix[off + 1]) * 184 + int(srcImage.Pix[off + 2]) * 18
+				off := (x*4 + y*srcImage.Stride)
+				a := int(srcImage.Pix[off+3])
+				g := int(srcImage.Pix[off+0])*54 + int(srcImage.Pix[off+1])*184 + int(srcImage.Pix[off+2])*18
 
 				if a >= alphaThreshold || g >= grayRGBAThreshold {
-					dstImage.Pix[x + y * dstImage.Stride] = 255
+					dstImage.Pix[x+y*dstImage.Stride] = 255
 				}
 			}
 		}
@@ -173,8 +173,8 @@ type point struct {
 	ISqDist int
 }
 
-var inside = point{OX:0, OY:0, OSqDist:0, IX:1 << 15, IY:1 << 15, ISqDist:1 << 31}
-var outside = point{OX:1 << 15, OY:1 << 15, OSqDist:1 << 31, IX:0, IY:0, ISqDist:0}
+var inside = point{OX: 0, OY: 0, OSqDist: 0, IX: 1 << 15, IY: 1 << 15, ISqDist: 1 << 31}
+var outside = point{OX: 1 << 15, OY: 1 << 15, OSqDist: 1 << 31, IX: 0, IY: 0, ISqDist: 0}
 
 type field struct {
 	Width, Height int
@@ -182,8 +182,8 @@ type field struct {
 }
 
 func newField(width, height int) *field {
-	df := &field{Width:width, Height:height, Field:make([]point, width * height)}
-	for i := 0; i < width * height; i++ {
+	df := &field{Width: width, Height: height, Field: make([]point, width*height)}
+	for i := 0; i < width*height; i++ {
 		df.Field[i] = outside
 	}
 
@@ -191,15 +191,15 @@ func newField(width, height int) *field {
 }
 
 func newFieldFromImage(image *image.Gray) *field {
-	imageWidth := image.Bounds().Dx();
-	imageHeight := image.Bounds().Dy();
+	imageWidth := image.Bounds().Dx()
+	imageHeight := image.Bounds().Dy()
 
-	field := newField(imageWidth + 2, imageHeight + 2)
+	field := newField(imageWidth+2, imageHeight+2)
 
 	for y := 0; y < imageHeight; y++ {
 		for x := 0; x < imageWidth; x++ {
-			if image.Pix[x + y * image.Stride] >= 128 {
-				field.Field[(x + 1) + (y + 1) * field.Width] = inside
+			if image.Pix[x+y*image.Stride] >= 128 {
+				field.Field[(x+1)+(y+1)*field.Width] = inside
 			}
 		}
 	}
@@ -208,14 +208,14 @@ func newFieldFromImage(image *image.Gray) *field {
 }
 
 func updatePoint(d, s *point, xoff, yoff int) {
-	sqDist := (s.OX + xoff) * (s.OX + xoff) + (s.OY + yoff) * (s.OY + yoff)
+	sqDist := (s.OX+xoff)*(s.OX+xoff) + (s.OY+yoff)*(s.OY+yoff)
 	if sqDist < d.OSqDist {
 		d.OX = s.OX + xoff
 		d.OY = s.OY + yoff
 		d.OSqDist = sqDist
 	}
 
-	sqDistInside := (s.IX + xoff) * (s.IX + xoff) + (s.IY + yoff) * (s.IY + yoff)
+	sqDistInside := (s.IX+xoff)*(s.IX+xoff) + (s.IY+yoff)*(s.IY+yoff)
 	if sqDistInside < d.ISqDist {
 		d.IX = s.IX + xoff
 		d.IY = s.IY + yoff
@@ -224,27 +224,27 @@ func updatePoint(d, s *point, xoff, yoff int) {
 }
 
 func (df *field) fill() {
-	for y := 1; y < df.Height - 1; y++ {
-		for x := 1; x < df.Width - 1; x++ {
-			updatePoint(&df.Field[x + y * df.Width], &df.Field[(x - 1) + y * df.Width], -1, 0)      // left
-			updatePoint(&df.Field[x + y * df.Width], &df.Field[x + (y - 1) * df.Width], 0, -1)      // above
-			updatePoint(&df.Field[x + y * df.Width], &df.Field[(x - 1) + (y - 1) * df.Width], -1, -1) // above and left
-			updatePoint(&df.Field[x + y * df.Width], &df.Field[(x + 1) + (y - 1) * df.Width], 1, -1)  // above and right
+	for y := 1; y < df.Height-1; y++ {
+		for x := 1; x < df.Width-1; x++ {
+			updatePoint(&df.Field[x+y*df.Width], &df.Field[(x-1)+y*df.Width], -1, 0)      // left
+			updatePoint(&df.Field[x+y*df.Width], &df.Field[x+(y-1)*df.Width], 0, -1)      // above
+			updatePoint(&df.Field[x+y*df.Width], &df.Field[(x-1)+(y-1)*df.Width], -1, -1) // above and left
+			updatePoint(&df.Field[x+y*df.Width], &df.Field[(x+1)+(y-1)*df.Width], 1, -1)  // above and right
 		}
 		for x := df.Width - 2; x > 0; x-- {
-			updatePoint(&df.Field[x + y * df.Width], &df.Field[(x + 1) + y * df.Width], 1, 0) // right
+			updatePoint(&df.Field[x+y*df.Width], &df.Field[(x+1)+y*df.Width], 1, 0) // right
 		}
 	}
 
 	for y := df.Height - 2; y > 0; y-- {
 		for x := df.Width - 2; x > 0; x-- {
-			updatePoint(&df.Field[x + y * df.Width], &df.Field[(x + 1) + y * df.Width], 1, 0)      // right
-			updatePoint(&df.Field[x + y * df.Width], &df.Field[x + (y + 1) * df.Width], 0, 1)      // below
-			updatePoint(&df.Field[x + y * df.Width], &df.Field[(x + 1) + (y + 1) * df.Width], 1, 1)  // below and right
-			updatePoint(&df.Field[x + y * df.Width], &df.Field[(x - 1) + (y + 1) * df.Width], -1, 1) // below and left
+			updatePoint(&df.Field[x+y*df.Width], &df.Field[(x+1)+y*df.Width], 1, 0)      // right
+			updatePoint(&df.Field[x+y*df.Width], &df.Field[x+(y+1)*df.Width], 0, 1)      // below
+			updatePoint(&df.Field[x+y*df.Width], &df.Field[(x+1)+(y+1)*df.Width], 1, 1)  // below and right
+			updatePoint(&df.Field[x+y*df.Width], &df.Field[(x-1)+(y+1)*df.Width], -1, 1) // below and left
 		}
-		for x := 1; x < df.Width - 1; x++ {
-			updatePoint(&df.Field[x + y * df.Width], &df.Field[(x - 1) + y * df.Width], -1, 0) // left
+		for x := 1; x < df.Width-1; x++ {
+			updatePoint(&df.Field[x+y*df.Width], &df.Field[(x-1)+y*df.Width], -1, 0) // left
 		}
 	}
 }
--- main.go
+++ main.go
@@ -1,38 +1,49 @@
 package main
+
 import (
 	"fmt"
-	_ "image/png"
-	"os"
 	"github.com/nfnt/resize"
-	"image"
-	"image/png"
-	"image/draw"
 	"gopkg.in/alecthomas/kingpin.v2"
-	"math"
-	"log"
+	"image"
 	"image/color"
+	"image/draw"
+	"image/png"
+	_ "image/png"
+	"math"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
 )
 
 var (
 	app = kingpin.New("distancefield", "Generate distancefields from images.")
 
-	grayThresholdFlag = app.Flag("gray", "gray threshold (image color must be larger or equal than this to be \"inside\"").Default("16").Int()
-	alphaThresholdFlag = app.Flag("alpha", "alpha threshold (image alpha must be larger or equal than this to be \"inside\"").Default("32").Int()
-	oversampleFlag = app.Flag("oversample", "oversample image by this (must be power of 2)").Default("1").Int()
-	padFlag = app.Flag("pad", "image will be padded by this amount of pixels on all sides").Default("0").Int()
-	sourceImageFlag = app.Flag("source", "source image").Required().File()
+	// todo, add line on how colors are formatted "#RRGGBB" or "#AARRGGBB")
+	// todo, clean up color parsing (change all color inputs to "#..."
 
-	distanceFieldCmd = app.Command("distancefield", "save distancefield as image.")
-	distanceFieldCmdModeFlag = distanceFieldCmd.Flag("mode", "").Default("signed").Enum("unsigned", "signed", "signed2")
-	distanceFieldCmdScaleFlag = distanceFieldCmd.Flag("scale", "multiply stored distances.").Default("1.0").Float64()
+	grayThresholdFlag  = app.Flag("gray", "gray threshold (image color must be larger or equal than this to be \"inside\"").Default("16").Int()
+	alphaThresholdFlag = app.Flag("alpha", "alpha threshold (image alpha must be larger or equal than this to be \"inside\"").Default("32").Int()
+	oversampleFlag     = app.Flag("oversample", "oversample image by this (must be power of 2)").Default("1").Int()
+	padFlag            = app.Flag("pad", "image will be padded by this amount of pixels on all sides").Default("0").Int()
+	sourceImageFlag    = app.Flag("source", "source image").Required().File()
+
+	distanceFieldCmd                    = app.Command("distancefield", "save distancefield as image.")
+	distanceFieldCmdModeFlag            = distanceFieldCmd.Flag("mode", "").Default("signed").Enum("unsigned", "signed", "signed2")
+	distanceFieldCmdScaleFlag           = distanceFieldCmd.Flag("scale", "multiply stored distances.").Default("1.0").Float64()
 	distanceFieldCmdDestinationImageArg = distanceFieldCmd.Arg("dest", "destination image").Required().String()
 
-	glowCmd = app.Command("glow", "create glow from distancefield.")
-	glowCmdColorFlag = glowCmd.Flag("color", "color of glow in hex (ganerated image is single channel grayscale if not set)").Default("-1").Int32()
-	glowCmdOuterScaleFlag = glowCmd.Flag("oscale", "multiply outer distances.").Default("16.0").Float64()
-	glowCmdInnterScaleFlag = glowCmd.Flag("iscale", "multiply inner distances.").Default("64.0").Float64()
-	glowCmdGammaFlag = glowCmd.Flag("gamma", "gamma correction value").Default("2.0").Float64()
+	glowCmd                    = app.Command("glow", "create simple glow from distancefield.")
+	glowCmdColorFlag           = glowCmd.Flag("color", "color of glow in hex (\"0xrrggbb\"). If not set dest will be single channel grayscale").Default("-1").Int32()
+	glowCmdOuterScaleFlag      = glowCmd.Flag("oscale", "multiply outer distances.").Default("16.0").Float64()
+	glowCmdInnterScaleFlag     = glowCmd.Flag("iscale", "multiply inner distances.").Default("64.0").Float64()
+	glowCmdGammaFlag           = glowCmd.Flag("gamma", "gamma correction value").Default("2.0").Float64()
 	glowCmdDestinationImageArg = glowCmd.Arg("dest", "destination image").Required().String()
+
+	outlineCmd = app.Command("outline", "create colored outline from distancefield.")
+	//outlineCmdColorFlag = outlineCmd.Flag("color", "default color of outline in hex (\"0xrrggbb\")").Default("-1").Int32()
+	outlineCmdSampleFlag          = outlineCmd.Flag("color", "map dist to color (format: dist:color) use negative dist for inside.").Required().Strings()
+	outlineCmdDestinationImageArg = outlineCmd.Arg("dest", "destination image").Required().String()
 )
 
 func saveImage(i image.Image, path string) error {
@@ -48,11 +59,144 @@ func saveImage(i image.Image, path string) error {
 	return nil
 }
 
+func parseColor(hexColor string) (col color.NRGBA, err error) {
+	if len(hexColor) < 1 || hexColor[0] != '#' {
+		err = fmt.Errorf("Invalid color format ('%s'), expected first character to be a '#'", hexColor)
+	} else {
+		colval64, err := strconv.ParseUint(hexColor[1:], 16, 32)
+		if err == nil {
+			colval := int(colval64)
+
+			switch len(hexColor) {
+			case 4: // #rgb
+				col = color.NRGBA{uint8(((colval >> 8) & 0xf) * 0x11), uint8(((colval >> 4) & 0xf) * 0x11), uint8(((colval >> 0) & 0xf) * 0x11), 255}
+			case 5: // #argb
+				col = color.NRGBA{uint8(((colval >> 8) & 0xf) * 0x11), uint8(((colval >> 4) & 0xf) * 0x11), uint8(((colval >> 0) & 0xf) * 0x11), uint8(((colval >> 12) & 0xf) * 0x11)}
+			case 7: // #rrggbb
+				col = color.NRGBA{uint8(colval >> 16), uint8(colval >> 8), uint8(colval), 255}
+			case 9: // #aarrggbb
+				col = color.NRGBA{uint8(colval >> 16), uint8(colval >> 8), uint8(colval), uint8(colval >> 24)}
+			default:
+				err = fmt.Errorf("Invalid color format ('%s')", hexColor)
+			}
+		}
+	}
+
+	return
+}
+
+//type distColor struct{Dist float64; A, R, G, B float64;}
+type distColor struct {
+	Dist  float64
+	A     uint8
+	Color color.YCbCr
+}
+type distColors []distColor
+
+func interpolate(a float64, c1, c2 distColor) distColor {
+	a = (a - c1.Dist) / (c2.Dist - c1.Dist)
+	return distColor{
+		Dist: c1.Dist + (c2.Dist-c1.Dist)*a,
+		A:    c1.A + uint8((float64(c2.A)-float64(c1.A))*a),
+		Color: color.YCbCr{
+			Y:  c1.Color.Y + uint8((float64(c2.Color.Y)-float64(c1.Color.Y))*a),
+			Cb: c1.Color.Cb + uint8((float64(c2.Color.Cb)-float64(c1.Color.Cb))*a),
+			Cr: c1.Color.Cr + uint8((float64(c2.Color.Cr)-float64(c1.Color.Cr))*a)},
+	}
+}
+
+func (c distColors) interpolate(dist float64) distColor {
+	if dist <= c[0].Dist {
+		return c[0]
+	}
+
+	for i := 0; i < len(c)-1; i++ {
+		if dist <= c[i+1].Dist {
+			return interpolate(dist, c[i], c[i+1])
+		}
+	}
+
+	return c[len(c)-1]
+}
+
+func (c distColors) Len() int {
+	return len(c)
+}
+
+func (c distColors) Less(a, b int) bool {
+	return c[a].Dist < c[b].Dist
+}
+
+func (c distColors) Swap(a, b int) {
+	c[a], c[b] = c[b], c[a]
+}
+
+func RGBToYCbCr(rgba color.NRGBA) color.YCbCr {
+	y, cb, cr := color.RGBToYCbCr(rgba.R, rgba.G, rgba.B)
+	return color.YCbCr{y, cb, cr}
+}
+
+func makeOutline(field *Field, unparsedColors []string, destImagePath string) error {
+	colors := make(distColors, len(unparsedColors))
+	for i := 0; i < len(unparsedColors); i++ {
+		s := strings.Split(unparsedColors[i], ":")
+
+		if len(s) != 2 {
+			return fmt.Errorf("Invalid dist:color format ('%s')", unparsedColors[i])
+		}
+
+		dist, err := strconv.ParseFloat(s[0], 64)
+		if err != nil {
+			return err
+		}
+
+		rgba, err := parseColor(s[1])
+		if err != nil {
+			return err
+		}
+
+		colors[i] = distColor{dist, rgba.A, RGBToYCbCr(rgba)}
+	}
+
+	sort.Sort(colors)
+	//	fmt.Println(colors)
+	//	for d := -10.0; d<=20; d+= 1 {
+	//		fmt.Println(d, colors.interpolate(d))
+	//	}
+
+	dstImg := image.NewNRGBA(image.Rect(0, 0, field.Width, field.Height))
+
+	for y := 0; y < field.Height; y++ {
+		for x := 0; x < field.Width; x++ {
+			idist := field.Field[x+y*field.Width].InsideDist
+			odist := field.Field[x+y*field.Width].OutsideDist
+
+			var ycbcr distColor
+			if idist >= odist {
+				ycbcr = colors.interpolate(-idist)
+			} else {
+				ycbcr = colors.interpolate(odist)
+			}
+
+			r, g, b := color.YCbCrToRGB(ycbcr.Color.Y, ycbcr.Color.Cb, ycbcr.Color.Cr)
+
+			off := x*4 + y*dstImg.Stride
+			dstImg.Pix[off] = r
+			dstImg.Pix[off+1] = g
+			dstImg.Pix[off+2] = b
+			dstImg.Pix[off+3] = ycbcr.A
+		}
+	}
+
+	err := saveImage(dstImg, destImagePath)
+	return err
+}
+
 func main() {
 	command := kingpin.MustParse(app.Parse(os.Args[1:]))
 	_ = command
 
-	if *oversampleFlag < 1 || *oversampleFlag & (*oversampleFlag - 1) != 0 {
+	if *oversampleFlag < 1 || *oversampleFlag&(*oversampleFlag-1) != 0 {
 		fmt.Fprintln(os.Stderr, "invalid oversample option")
 		os.Exit(1)
 	}
@@ -67,7 +211,7 @@ func main() {
 	orgHeight := srcImg.Bounds().Dy()
 
 	if *oversampleFlag > 1 {
-		srcImg = resize.Resize(uint(orgWidth * *oversampleFlag), uint(orgHeight * *oversampleFlag), srcImg, resize.MitchellNetravali)
+		srcImg = resize.Resize(uint(orgWidth**oversampleFlag), uint(orgHeight**oversampleFlag), srcImg, resize.MitchellNetravali)
 	}
 
 	monoImage := NewMonochromeFromTreshold(srcImg, *grayThresholdFlag, *alphaThresholdFlag)
@@ -92,24 +236,24 @@ func main() {
 		case "unsigned":
 			for y := 0; y < field.Height; y++ {
 				for x := 0; x < field.Width; x++ {
-					dist := math.Min(field.Field[x + y * field.Width].OutsideDist * *distanceFieldCmdScaleFlag, 255)
-					dstImg.Pix[x + y * dstImg.Stride] = uint8(dist)
+					dist := math.Min(field.Field[x+y*field.Width].OutsideDist**distanceFieldCmdScaleFlag, 255)
+					dstImg.Pix[x+y*dstImg.Stride] = uint8(dist)
 				}
 			}
 
 		case "signed":
 			for y := 0; y < field.Height; y++ {
 				for x := 0; x < field.Width; x++ {
-					idist := field.Field[x + y * field.Width].InsideDist
-					odist := field.Field[x + y * field.Width].OutsideDist
+					idist := field.Field[x+y*field.Width].InsideDist
+					odist := field.Field[x+y*field.Width].OutsideDist
 					if idist == odist {
-						dstImg.Pix[x + y * dstImg.Stride] = 128
+						dstImg.Pix[x+y*dstImg.Stride] = 128
 					} else if idist < odist {
-						dist := math.Max((128 - odist * *distanceFieldCmdScaleFlag), 0)
-						dstImg.Pix[x + y * dstImg.Stride] = uint8(dist)
+						dist := math.Max((128 - odist**distanceFieldCmdScaleFlag), 0)
+						dstImg.Pix[x+y*dstImg.Stride] = uint8(dist)
 					} else {
-						dist := math.Min((128 + idist * *distanceFieldCmdScaleFlag), 255)
-						dstImg.Pix[x + y * dstImg.Stride] = uint8(dist)
+						dist := math.Min((128 + idist**distanceFieldCmdScaleFlag), 255)
+						dstImg.Pix[x+y*dstImg.Stride] = uint8(dist)
 					}
 				}
 			}
@@ -117,16 +261,16 @@ func main() {
 		case "signed2":
 			for y := 0; y < field.Height; y++ {
 				for x := 0; x < field.Width; x++ {
-					idist := field.Field[x + y * field.Width].InsideDist
-					odist := field.Field[x + y * field.Width].OutsideDist
+					idist := field.Field[x+y*field.Width].InsideDist
+					odist := field.Field[x+y*field.Width].OutsideDist
 					if idist == odist {
-						dstImg.Pix[x + y * dstImg.Stride] = 0
+						dstImg.Pix[x+y*dstImg.Stride] = 0
 					} else if idist < odist {
-						dist := math.Max((-odist * *distanceFieldCmdScaleFlag) - 0.5, -128)
-						dstImg.Pix[x + y * dstImg.Stride] = uint8(dist)
+						dist := math.Max((-odist**distanceFieldCmdScaleFlag)-0.5, -128)
+						dstImg.Pix[x+y*dstImg.Stride] = uint8(dist)
 					} else {
-						dist := math.Min((idist * *distanceFieldCmdScaleFlag) + 0.5, 127)
-						dstImg.Pix[x + y * dstImg.Stride] = uint8(int8(dist))
+						dist := math.Min((idist**distanceFieldCmdScaleFlag)+0.5, 127)
+						dstImg.Pix[x+y*dstImg.Stride] = uint8(int8(dist))
 					}
 				}
 			}
@@ -134,7 +278,7 @@ func main() {
 
 		err := saveImage(dstImg, *distanceFieldCmdDestinationImageArg)
 		if err != nil {
-			log.Panicf("failed to save %s: %s\n", *distanceFieldCmdDestinationImageArg, err)
+			fmt.Fprintf(os.Stderr, "failed to save %s: %s\n", *distanceFieldCmdDestinationImageArg, err)
 			os.Exit(2)
 		}
 
@@ -144,14 +288,14 @@ func main() {
 		var stride int
 		var pix []uint8
 		if *glowCmdColorFlag < 0 {
-			grayImage :=image.NewGray(image.Rect(0, 0, field.Width, field.Height))
+			grayImage := image.NewGray(image.Rect(0, 0, field.Width, field.Height))
 			dstImg = grayImage
 			pixwidth = 1
 			stride = grayImage.Stride
 			pix = grayImage.Pix
 		} else {
 			colorImg := image.NewNRGBA(image.Rect(0, 0, field.Width, field.Height))
-			draw.Over.Draw(colorImg, colorImg.Rect, image.NewUniform(color.RGBA{uint8(*glowCmdColorFlag>>16), (uint8(*glowCmdColorFlag>>8)), uint8(*glowCmdColorFlag), 255}), image.ZP)
+			draw.Over.Draw(colorImg, colorImg.Rect, image.NewUniform(color.RGBA{uint8(*glowCmdColorFlag >> 16), (uint8(*glowCmdColorFlag >> 8)), uint8(*glowCmdColorFlag), 255}), image.ZP)
 			dstImg = colorImg
 			pixwidth = 4
 			stride = colorImg.Stride
@@ -160,18 +304,25 @@ func main() {
 
 		for y := 0; y < field.Height; y++ {
 			for x := 0; x < field.Width; x++ {
-				idist := field.Field[x + y * field.Width].InsideDist
-				odist := field.Field[x + y * field.Width].OutsideDist
+				idist := field.Field[x+y*field.Width].InsideDist
+				odist := field.Field[x+y*field.Width].OutsideDist
 				dist := math.Max(idist**glowCmdInnterScaleFlag, odist**glowCmdOuterScaleFlag)
-				dist = math.Pow(dist/256, *glowCmdGammaFlag)*256
-				dist = math.Min(dist + 0.5, 255)
-				pix[x*pixwidth + y * stride] = 255-uint8(dist)
+				dist = math.Pow(dist/256, *glowCmdGammaFlag) * 256
+				dist = math.Min(dist+0.5, 255)
+				pix[x*pixwidth+y*stride] = 255 - uint8(dist)
 			}
 		}
 
 		err := saveImage(dstImg, *glowCmdDestinationImageArg)
 		if err != nil {
-			log.Panicf("failed to save %s: %s\n", *glowCmdDestinationImageArg, err)
+			fmt.Fprintf(os.Stderr, "failed to save %s: %s\n", *glowCmdDestinationImageArg, err)
+			os.Exit(2)
+		}
+
+	case outlineCmd.FullCommand():
+		err = makeOutline(field, *outlineCmdSampleFlag, *outlineCmdDestinationImageArg)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "failed to generate outline: %s\n", err)
 			os.Exit(2)
 		}
 	}
